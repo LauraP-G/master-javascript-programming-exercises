9d0779f73035ee5ce236fc0d7682f2727f27e2a5
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-05-23 09:25:32 +0000

    Realizados hasta ejercicios numero 43

--- exercises/026-computeAreaOfATriangle/app.js
+++ exercises/026-computeAreaOfATriangle/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+let computeAreaOfATriangle = (base, altura) => (base*altura)/2
+
+
+console.log(computeAreaOfATriangle(4,6))
--- exercises/028-square/app.js
+++ exercises/028-square/app.js
@@ -1,1 +1,6 @@
 // Write your function here
+let square = (num) => num **2
+
+
+let output = square(5);
+console.log(output); // --> 25
