7e115155faebb56567e0d61fe5761ab4273b5dfc
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-05-24 08:11:40 +0000

    resolviendo ejercicios hasta el número 60

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,14 @@
 // Write your function here
+let computeAverageOfNumbers = (arr) => {
+    let averageArr = 0;
+    if(arr.length == 0) return 0;
+    for (let element of arr){
+        averageArr += element
+
+    }
+    return averageArr / arr.length
+}
+
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
